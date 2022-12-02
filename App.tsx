8a482b770fcd61a79c37bba5a63016d51c52d246
sachin <sachinrs1310@gmail.com>
2022-12-02 14:18:24 +0530

    initial commit

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import RegisterNotification from './RegisterNotification';
+const App = () => {
+
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignSelf: 'center' }}>
+      <Text >React Native Notifications</Text>
+    </View>
+  );
+};
+
+export default App;
