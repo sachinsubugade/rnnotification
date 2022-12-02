d82d5aabe00cd21ae8e5d82b30a60b2edd6de2c2
sachin <sachinrs1310@gmail.com>
2022-12-02 18:12:27 +0530

    updated manifest

--- App.tsx
+++ App.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 import RegisterNotification from './RegisterNotification';
+import { Notifications } from 'react-native-notifications'
 const App = () => {
+  Notifications.registerRemoteNotifications();
 
+  // Notifications.events().registerRemoteNotificationsRegistered((event) => {
+  //   // TODO: Send the token to my server so it could send back push notifications...
+  //   console.log("Device Token Received", event.deviceToken);
+  // });
+  // Notifications.events().registerRemoteNotificationsRegistrationFailed((event) => {
+  //   console.error(event);
+  // });
+  // Notifications.ios.checkPermissions().then((currentPermissions) => {
+  //   console.log(currentPermissions)
+  // }).catch(error => console.log(error))
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignSelf: 'center' }}>
       <Text >React Native Notifications</Text>
--- RegisterNotification.tsx
+++ RegisterNotification.tsx
@@ -1,21 +1,12 @@
-import React, { Component } from 'react';
-import { Notifications } from 'react-native-notifications';
+import { View, Text } from 'react-native'
+import React from 'react'
 
-class RegisterNotification extends Component {
-    constructor(props) {
-        super(props);
-        Notifications.registerRemoteNotifications();
-
-        Notifications.events().registerNotificationReceivedForeground((notification: Notification, completion) => {
-            console.log(`Notification received in foreground: ${notification.title} : ${notification.body}`);
-            completion({ alert: false, sound: false, badge: false });
-        });
-
-        Notifications.events().registerNotificationOpened((notification: Notification, completion) => {
-            console.log(`Notification opened: ${notification.payload}`);
-            completion();
-        });
-    }
+const RegisterNotification = () => {
+    return (
+        <View>
+            <Text>RegisterNotification</Text>
+        </View>
+    )
 }
 
 export default RegisterNotification
